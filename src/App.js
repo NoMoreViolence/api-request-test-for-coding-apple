0b293d549c32ea14977de7840ac8bc9161fa39fc
NoMoreViolence <ljh86029926@gmail.com>
2019-04-20 17:29:27 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    requestArray: [],
+    pending: false
+  }
+
+  apiRequest = () => {
+    if (this.state.pending === true) {
+      return;
+    }
+
+    this.setState({
+      pending: true
+    },
+      () => {
+        axios.get('https://baconipsum.com/api/?type=meat-and-filter').then(res => {
+          this.setState({
+            requestArray: [...this.state.requestArray, ...res.data],
+            pending: false
+          });
+          console.log(res.data);
+        }).catch(err => {
+          this.setState({
+            pending: false
+          });
+          console.log(err);
+        })
+      }
+    )
+  }
+
+  componentDidMount() {
+    this.apiRequest();
+  }
+
   render() {
+    const renderArray = (arr) => {
+      return arr.map((data, idx) => {
+        return <div key={idx}>{data}</div>
+      });
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {renderArray(this.state.requestArray)}
+        <button onClick={this.apiRequest}>Click me</button>
       </div>
     );
   }
